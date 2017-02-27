6f116b7f6f7914b1114749dbb6b219e00876368c
gislileifs <gisli.leifsson@gmail.com>
2017-02-27 02:57:43 +0000

    Bætti við mongo.properties

--- src/main/java/gisli/configuration/MongoConfig.java
+++ src/main/java/gisli/configuration/MongoConfig.java
@@ -1,5 +1,6 @@
 package gisli.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,21 +12,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 //@EnableMongoRepositories(basePackages="gisli.model")
 @EnableMongoRepositories("gisli")
+@PropertySource("classpath:mongo.properties")
 public class MongoConfig extends AbstractMongoConfiguration {
 
+	@Autowired
+	Environment env;
+	
 	@Override
 	public String getDatabaseName() {
-		return "mydb";
+		return env.getProperty("mongo.db.name");
 	}
 
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		return new MongoClient("192.168.0.106");
-	}
+		String host = env.getProperty("mongo.host");
+		return new MongoClient(host);	}
 
 }
