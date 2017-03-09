9add64e55e6c3c7678137ab0e6d78f18876fe694
gislileifsson <gisli.leifsson@gmail.com>
2017-03-09 20:35:39 +0000

    Bætti við log4j

--- src/main/java/gisli/configuration/SecurityConfig.java
+++ src/main/java/gisli/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import gisli.controller.SecUserDetailsService;
 
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -31,11 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 */	
     @Autowired
     PersistentTokenRepository repository;
+    
+	private static final Logger logger = Logger.getLogger(SecurityConfig.class);
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		System.out.println("Configuring security");
+		logger.debug("Configuring security");
 		
 	  http.authorizeRequests()
 	  	.antMatchers("/login", "/resources/**", "/images/**").permitAll()
