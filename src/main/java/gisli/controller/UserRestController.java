61ceda68b0a9429de49b3cdb1a7e9fd5e613b1bc
gislileifs <gisli.leifsson@gmail.com>
2017-04-11 13:47:45 +0000

    Bætti við smá loggun

--- src/main/java/gisli/controller/UserRestController.java
+++ src/main/java/gisli/controller/UserRestController.java
@@ -2,6 +2,7 @@ package gisli.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import gisli.configuration.SecurityConfig;
 import gisli.model.User;
 import gisli.model.WineLogEntry;
 import gisli.service.UserService;
 
 @RestController
 public class UserRestController {
-
+	private static final Logger logger = Logger.getLogger(UserRestController.class);
+	
 	@Autowired
 	UserService userService;
 	
--- src/main/java/gisli/service/UserServiceImpl.java
+++ src/main/java/gisli/service/UserServiceImpl.java
@@ -14,25 +14,19 @@ public class UserServiceImpl implements UserService {
 
 	MyMongo mongo = new MyMongo(); 
 	
-	@Override
 	public void saveUser(User user) {
 		mongo.save(user);
 		System.out.println("After mongo.save. User actually saved. " + user.toString());
 	}
 
-	@Override
 	public User getUser(String id) {
 		return mongo.getUser(id);
 	}
 
-	@Override
 	public List<User> getAllUsers() {
-		System.out.println("UserService.getAllUsers");
-		System.out.println("UserServiceImpl: getAllUsers");
 		return mongo.getUsers();		
 	}
 
-	@Override
 	public void deleteUser(String id) {
 		// TODO Auto-generated method stub
 		mongo.deleteUser(id);		
