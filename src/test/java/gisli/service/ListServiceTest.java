8ba774702e3148c299180e09a94e8c6a71de9e56
gislileifs <gisli.leifsson@gmail.com>
2017-04-15 14:23:39 +0000

    ListServiceTest

---
+++ src/test/java/gisli/service/ListServiceTest.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package gisli.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author gislileifsson
+ *
+ */
+public class ListServiceTest {
+
+	ListService service = new ListServiceImpl();
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link gisli.service.ListService#saveList(gisli.model.ItemList)}.
+	 */
+	@Test
+	public final void testSaveList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gisli.service.ListService#getList(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gisli.service.ListService#getLists()}.
+	 */
+	@Test
+	public final void testGetLists() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gisli.service.ListService#deleteList(java.lang.String)}.
+	 */
+	@Test
+	public final void testDeleteList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
